ce29e3a568c7034edbab7faf92dd28fc6baa9c19
Cameron Jarvis <cameron@clcpro.com>
2024-09-04 16:32:23 -0400

    Create FeatureA.java
    An initial pass of dummy code that will upset CheckStyles.

---
+++ src/FeatureA.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class FeatureA {
+
+	public static void main(String[] args) {
+		FeatureA a = new FeatureA();
+		a.Test();
+	}
+
+	void Test() {
+		System.out.println("This is a test of legal Java code that will upset Check-Styles.");
+	}
+	
+}
