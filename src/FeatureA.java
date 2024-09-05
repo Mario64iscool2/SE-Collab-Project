a5196c1a222b63a07c218c768c41475af0418fd3
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2024-09-04 20:19:19 -0400

    Update FeatureA.java
    added a complex boolean expression as a test involving rolling dice

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -8,6 +8,13 @@ public class FeatureA {
 
 	void test() {
 		System.out.println("This is a test of legal Java code that will upset Check-Styles.");
+		boolean diceoneisasix = false 
+		boolean dicetwoisasix = true
+		if(diceoneisasix && dicetwoisasix){
+			System.out.println("You win a prize");
+		} else {
+			System.out.println("You don't win a prize");
+		}
 	}
 	
 }
