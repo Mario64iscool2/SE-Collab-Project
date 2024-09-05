48f89586c1009c7166d533b7a39916ff89a244d6
Cameron Jarvis <cameron@clcpro.com>
2024-09-05 10:15:41 -0400

    Checkstyle should now pass

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -1,12 +1,12 @@
-import java.io.*;
+
 public class FeatureA {
 
 	public static void main(String[] args) {
 		FeatureA a = new FeatureA();
-		a.Test();
+		a.test();
 	}
 
-	void Test() {
+	void test() {
 		System.out.println("This is a test of legal Java code that will upset Check-Styles.");
 	}
 	
