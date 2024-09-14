1e6bc3b29d8b4a56003884630548885c501f8437
Mario64iscool2 <cameron@clcpro.com>
2024-09-14 19:58:44 -0400

    Start API Work on Computation Engine
    Starting interface work on the exposed methods of the compute engine.

---
+++ src/IEngine.java
@@ -0,0 +1,23 @@
+
+/**
+ * @author mario64iscool2
+ *
+ */
+public interface IEngine {
+	/**
+	 * Sets the output delimiters (characters between input/result pairs and between results)
+	 * @param pair delimits the input integer from the result
+	 * @param end delimits results from one another
+	 */
+	void setDelimiters(char pair, char end);
+	/**
+	 * Retrieves the next output string
+	 * @return a string containing the input for this computation, the delimiting character, and the resultant output.
+	 */
+	String outputNext();
+	/**
+	 * Whether or not the active computation(s) have finished
+	 * @return a boolean truth value of whether or not the computation engine has completed the computation.
+	 */
+	boolean hasFinished();
+}
