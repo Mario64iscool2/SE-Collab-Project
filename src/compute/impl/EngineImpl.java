70bbbf79d891111afd8b2039b91d49a5ab049148
Cameron Jarvis <cameron@clcpro.com>
2024-09-26 12:38:45 -0400

    More Checkstyles Fixes

--- src/compute/impl/EngineImpl.java
+++ src/compute/impl/EngineImpl.java
@@ -1,19 +1,19 @@
 package compute.impl;
+
 import compute.Engine;
 import compute.ICompute;
 import data.OutputSource;
 
 public class EngineImpl extends Engine {
-	
-	public EngineImpl(ICompute compute)
-	{
+
+	public EngineImpl(ICompute compute) {
 		computation = compute;
 	}
-	
+
 	@Override
 	public String compute(int i, OutputSource dst) {
 		long result = computation.compute(i);
-		
+
 		return Long.toString(result);
 	}
 
