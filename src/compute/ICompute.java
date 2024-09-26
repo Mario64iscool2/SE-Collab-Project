4723624dab4120c67fa38e3e02ca18a0d5cdff09
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2024-09-26 11:15:57 -0700

    Adding computation (#8)
    * Update ICompute.java
    
    * added method to engine.java
    
    I dont think we can define methods in interfaces
    
    * added second method
    
    added second method to return first greater prime number
    
    * Reorganized Code, Corrected Compilation
    
    Removed the attempted implementation from the abstract class, and relocated it to the actual implementation.
    Fixed compilation.
    @gavinbell123 If your stuff doesn't compile locally, please debug it if you can.
    If you're not using a development environment such as Eclipse, I implore you to start doing so.
    
    * Fixed Formatting in NextPrimeCompute
    
    Checkstyles will be pleased.
    
    ---------
    
    Co-authored-by: Cameron Jarvis <cameron@clcpro.com>

--- src/compute/ICompute.java
+++ src/compute/ICompute.java
@@ -8,6 +8,7 @@ public interface ICompute {
 
 	/**
 	 * This will run the computation to be implemented
+	 * @apiNote Any user of a class implementing <code>ICompute</code> should just expect that compute is valid for all i.
 	 * @param i the integer input
 	 * @return the resultant long
 	 */
--- src/compute/impl/NextPrimeCompute.java
+++ src/compute/impl/NextPrimeCompute.java
@@ -1,12 +1,28 @@
 package compute.impl;
+
 import compute.ICompute;
 
 public class NextPrimeCompute implements ICompute {
 
 	@Override
 	public long compute(int i) {
-		// TODO Auto-generated method stub
-		return -1;
+		if (isPrime(i)) {
+			return i;
+		} else {
+			return -1;
+		}
+	}
+
+	boolean isPrime(int x) {
+		if (x <= 1) {
+			return false;
+		}
+		for (int i = 2; i < x; i++) {
+			if (x % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
