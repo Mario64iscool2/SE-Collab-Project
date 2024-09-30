7507196437859a0cc37a9f7e27be097723c9a0fd
Melataylor519 <melataylor519@gmail.com>
2024-09-30 17:34:21 -0400

    Completed DataStorageSystemTest

--- test/data/DataStorageSystemTest.java
+++ test/data/DataStorageSystemTest.java
@@ -1,19 +1,26 @@
-package data;
+import data.*;
 
 import org.mockito.Mockito;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import data.impl.DataStorageSystemImpl;
+import utils.Status;
 
 public class DataStorageSystemTest {
 
 	@Test
 	public void testDataStorageSystem() {
-		DataStorageSystemImpl mockDataSystem = Mockito.mock(DataStorageSystemImpl.class);
-		// TODO
-		// when(mockDataSystem.hasFinished()).thenReturn(true);
+		//mock parameters
+		InputSource mockInput = Mockito.mock(InputSource.class);
+		OutputSource mockOutput = Mockito.mock(OutputSource.class);
+		
+		DataStorageSystem dataStore = new DataStorageSystemImpl(mockInput, mockOutput);
+		
+		Assertions.assertEquals(4L, dataStore.read(mockInput));
+		
+		Assertions.assertEquals(Status.OK, dataStore.appendSingleResult(mockOutput, "result"));
 	}
 }
