9deab4777f4a7d95e695665ad0956065bea312f9
Melataylor519 <melataylor519@gmail.com>
2024-10-05 09:33:44 -0400

    Update EngineTest
    Properly tests Engine and fails as intended

--- test/compute/EngineTest.java
+++ test/compute/EngineTest.java
@@ -1,17 +1,38 @@
 package compute;
 
-import static org.mockito.Mockito.mock;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import compute.impl.EngineImpl;
+import data.InputSource;
+import data.OutputSource;
 
 public class EngineTest {
 
 	@Test
 	public void testEngine() {
-		EngineImpl eng = new EngineImpl(mock());
-		eng.compute(1, mock());
+		//mock parameters
+		ICompute mockICompute = Mockito.mock(ICompute.class);
+		
+		//mock class
+		Engine engine = new EngineImpl(mockICompute);
+		
+		//mock Output
+		OutputSource dst = Mockito.mock(OutputSource.class);
+		
+		//test compute(int i, OutputSource dst)
+		Assertions.assertEquals("result", engine.compute(7, dst));
+		
+		//mock input
+		InputSource<?> src = Mockito.mock(InputSource.class);
+		
+		//test compute(InputSource<?> src, OutputSource dst)
+		Assertions.assertEquals("result", engine.compute(src, dst));
+		
+		
+	
 	}
 
 }
