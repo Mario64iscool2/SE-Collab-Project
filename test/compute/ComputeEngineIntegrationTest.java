b5cf8ac6c1de1eeafe3659d11300a138b5d11726
Mario64iscool2 <cameron@clcpro.com>
2024-10-09 16:46:23 -0400

    Working on ComputeEngineIntegrationTest
    Wrote in the additional testing, though I am unsure on how to run the tests to verify that they work.

--- test/compute/ComputeEngineIntegrationTest.java
+++ test/compute/ComputeEngineIntegrationTest.java
@@ -1,7 +1,9 @@
 package compute;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import compute.impl.EngineImpl;
@@ -14,23 +16,25 @@ public class ComputeEngineIntegrationTest {
 
 	@Test
 	public void testComputeEngine() {
-		
+
 		ICompute iCompute = new NextPrimeCompute();
 		Engine engine = new EngineImpl(iCompute);
-		
-		InMemoryDataStore dataStore = new InMemoryDataStore();
-		
+
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(1);
 		list.add(10);
 		list.add(25);
-		
+
 		InMemoryInput input = new InMemoryInput(list);
 		InMemoryOutput output = new InMemoryOutput();
-		
-		//compute
-		
-		//use assertEquals() to test if it was successful
-		
+
+		InMemoryDataStore dataStore = new InMemoryDataStore(input, output);
+
+		// compute
+		String computationResult = engine.compute(input, output);
+
+		// use assertEquals() to test if it was successful
+		Assertions.assertEquals(output.getOutput(),
+				new ArrayList<>(Arrays.asList(computationResult.split(Character.toString(engine.pair)))));
 	}
 }
